cc85e81cf0bb3174e661eadde68a20a1bcfc21d4
hundredark <hundredark@gmail.com>
2022-10-27 15:00:12 +0800

    Improve config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,23 @@
 import { defaultTheme } from 'vuepress'
 
 export default  {
+  title: 'MVM User Handbook',
+  description: 'A handbook for users to manage asset with MVM Bridge',
+  head: [['link', { rel: 'icon', href: '/images/favicon.png' }]],
   theme: defaultTheme({
+    logo: '/images/logo.svg',
+    lastUpdated: false,
+    contributors: false,
     // default theme config
     navbar: [
       {
         text: 'Home',
         link: '/',
       },
+      {
+        text: 'MVM Bridge',
+        link: 'https://bridge.mvm.app'
+      }
     ],
     sidebar: [
       {
